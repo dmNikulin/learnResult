a721da66b3017ed9a52d6cce732b3146e5f0cf5d
Dmitrii Nikulin <skygift.work@gmail.com>
2023-08-30 12:34:43 +0300

    First Step

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+string[] library = {"Лисица", "Кот", "Тигр", "Кит", "34"};
+
+string[] findSymbols (string[] words, int quantity)
+{
+    string[] selectionResult = new string[words.Length];
+    int length = words.Length();
+
+    for (int i = 0; i < length; i++)
+    {
+        if (words[i].Length <= quantity)
+        {
+            selectionResult[i] = words[i];
+        }
+    }
+}
+
