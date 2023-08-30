a6b6e09921c19a59802af925525ee037f0e4b3a3
Dmitrii Nikulin <skygift.work@gmail.com>
2023-08-30 12:47:45 +0300

    Work version

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ string[] library = {"Лисица", "Кот", "Тигр", "Кит", "34"};
 string[] findSymbols (string[] words, int quantity)
 {
     string[] selectionResult = new string[words.Length];
-    int length = words.Length();
+    int length = words.Length;
 
     for (int i = 0; i < length; i++)
     {
@@ -14,5 +14,20 @@ string[] findSymbols (string[] words, int quantity)
             selectionResult[i] = words[i];
         }
     }
+
+    return selectionResult;
 }
 
+void showList(string[] list)
+{
+    int length = list.Length;
+
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"{list[i]}, ");
+    }
+}
+
+string[] results = findSymbols(library, 3);
+
+showList(results);
